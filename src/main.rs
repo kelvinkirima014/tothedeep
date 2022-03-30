60732043dbca3c7716802fd3dde899e81d8e746f
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-30 11:21:15 +0300

    implement structured logs with backtrace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use color_eyre::Report;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     setup()?;
 
-    println!("Hello, world!");
+    info!("Hello, world!");
 
     Ok(())
 }
@@ -14,6 +16,14 @@ fn setup() -> Result<(), Report> {
         std::env::set_var("RUST_LIB_BACKTRACE", "1");
     }
     color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
     Ok(())
 }
