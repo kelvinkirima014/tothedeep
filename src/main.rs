fc32960e474c339b94ba80e418f61a1c59c7b042
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-04-01 17:24:33 +0300

    fetch URLs for website

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use color_eyre::Report;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
+use reqwest::Client;
+
+pub const URL_1: &str = "https://www.kirima.xyz/post/sha256";
+pub const URL_2: &str = "https://www.kirima.xyz/post/gentleintrosolana";
+
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     setup()?;
 
     info!("Hello, world!");
+
+    let client = Client::new();
+    let url = "https://www.kirima.xyz/";
+
+    let res = client.get(url).send().await?.error_for_status()?;
+    info!(%url, content_type = ?res.headers().get("content-type"));
 
     Ok(())
 }
@@ -25,5 +36,11 @@ fn setup() -> Result<(), Report> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
+    Ok(())
+}
+
+async fn fetch_url(client: &Client, url: &str) -> Result<(), Report> {
+    let res = client.get(url).send().await?.error_for_status()?;
+    info!(%url, content_type = ?res.headers().get("content-type"));
     Ok(())
 }
