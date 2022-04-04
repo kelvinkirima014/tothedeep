6713be075680ca2ae163a8fec97c2f47b9c3b3f6
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-04-04 10:10:44 +0300

    cause trouble with unsafe

--- src/testfuture.rs
+++ src/testfuture.rs
@@ -12,6 +12,9 @@ impl Future for TestFuture {
 	type Output = ();
 
 	fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
-		panic!("Oh noes");
+		unsafe {
+			*(0xF00D as *mut u64) = 0x0;
+		}
+		unreachable!();
 	}
 }
